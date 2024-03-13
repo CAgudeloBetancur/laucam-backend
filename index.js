1bb817bf7a298bc8e50843254bd8d4e8e125c173
Laura Blandon <laurablandonm@gmail.com>
2024-03-13 17:33:31 -0500

    configuracion incial: servidor y conexiona mongodb

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola soy una rama");
+import express from "express";
+import cors from "cors";
+import { getConnection } from "./db/mongodb_conection.js";
+
+const app = express();
+const port = 4001;
+app.use(cors());
+getConnection();
+app.use(express.json());
+app.listen(port,()=>{ 
+    console.log("app en http://localhost:" + port);
+});
